7fc625cee0db93e5b34c7686f73eb83c0b32a901
Lautaro Maza <lautarodamian33@gmail.com>
2025-08-25 12:07:48 -0300

    soluciones

--- components/dark/home-main/Portfolio.vue
+++ components/dark/home-main/Portfolio.vue
@@ -1,135 +1,90 @@
 <template>
-  <section class="inter-fixed-text section-padding">
+  <div></div>
+</template>
+
+<script setup>
+// Componente comentado temporalmente
+</script>
+
+<!-- 
+<template>
+  <section class="portfolio section-padding sub-bg">
     <div class="container">
-      <div class="sec-head mb-80">
-        <h6 class="sub-title main-color mb-25">Our Portfolio</h6>
-        <div class="bord pt-25 bord-thin-top d-flex justify-content-end">
-          <h2 class="fw-600 d-rotate wow">
-            <span class="rotate-text">
-              Our featured <span class="fw-200">projects</span>
-            </span>
-          </h2>
-        </div>
-      </div>
-    </div>
-    <div class="container position-re">
-      <div class="links-img">
-        <div class="row">
-          <div class="col-lg-4 items">
-            <div class="item">
-              <div
-                @mouseenter="handleMouseEnter"
-                @mouseleave="handleMouseLeave"
-                class="img"
-                data-tab="tab-1"
-              >
-                <img src="/dark/assets/imgs/works/1/1.jpg" alt="" />
-                <a href="/dark/project1" class="link-overlay"></a>
-              </div>
-              <div class="cont">
-                <span class="tag">Branding</span>
-                <h2>Earthmade Aroma</h2>
+      <div class="row">
+        <div class="col-lg-4">
+          <div class="item sub-bg mb-40">
+            <div class="img">
+              <img src="/dark/assets/imgs/works/1/1.jpg" alt="" />
+              <div class="tag sub-bg">
+                <span>Envato</span>
               </div>
             </div>
-          </div>
-          <div class="col-lg-4 items">
-            <div class="item">
-              <div
-                @mouseenter="handleMouseEnter"
-                @mouseleave="handleMouseLeave"
-                class="img"
-                data-tab="tab-2"
+            <div class="cont">
+              <h5>
+                <a href="/dark/project-details">
+                  Creative advertising in our life became a info noise
+                </a>
+              </h5>
+              <a
+                href="/dark/project-details"
+                class="d-flex align-items-center mt-30"
               >
-                <img src="/dark/assets/imgs/works/1/2.jpg" alt="" />
-                <a href="/dark/project2" class="link-overlay"></a>
-              </div>
-              <div class="cont">
-                <span class="tag">Branding</span>
-                <h2>Blom Air Purifier</h2>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-4 items">
-            <div class="item">
-              <div
-                @mouseenter="handleMouseEnter"
-                @mouseleave="handleMouseLeave"
-                class="img"
-                data-tab="tab-3"
-              >
-                <img src="/dark/assets/imgs/works/1/3.jpg" alt="" />
-                <a href="/dark/project3" class="link-overlay"></a>
-              </div>
-              <div class="cont">
-                <span class="tag">Branding</span>
-                <h2>Bank Rebranding</h2>
-              </div>
+                <span class="text mr-15">Read More</span>
+                <span class="ti-arrow-top-right"></span>
+              </a>
             </div>
           </div>
         </div>
-      </div>
-      <div class="links-text">
-        <ul class="rest">
-          <li id="tab-1">
-            <span class="tag">Branding</span>
-            <h2>Earthmade Aroma</h2>
-          </li>
-          <li id="tab-2">
-            <span class="tag">Branding</span>
-            <h2>Blom Air Purifier</h2>
-          </li>
-          <li id="tab-3">
-            <span class="tag">Branding</span>
-            <h2>Bank Rebranding</h2>
-          </li>
-        </ul>
-      </div>
-    </div>
-    <div class="container mt-40">
-      <div class="sec-bottom mt-100">
-        <div class="main-bg d-flex align-items-center">
-          <p>Here are some select projects that showcase.</p>
-          <a
-            href="/dark/portfolio-metro"
-            class="butn main-color ml-40 underline"
-          >
-            <span>View All</span>
-          </a>
+        <div class="col-lg-4">
+          <div class="item sub-bg mb-40">
+            <div class="img">
+              <img src="/dark/assets/imgs/works/1/2.jpg" alt="" />
+              <div class="tag sub-bg">
+                <span>Envato</span>
+              </div>
+            </div>
+            <div class="cont">
+              <h5>
+                <a href="/dark/project-details">
+                  Creative advertising in our life became a info noise
+                </a>
+              </h5>
+              <a
+                href="/dark/project-details"
+                class="d-flex align-items-center mt-30"
+              >
+                <span class="text mr-15">Read More</span>
+                <span class="ti-arrow-top-right"></span>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="item sub-bg mb-40">
+            <div class="img">
+              <img src="/dark/assets/imgs/works/1/3.jpg" alt="" />
+              <div class="tag sub-bg">
+                <span>Envato</span>
+              </div>
+            </div>
+            <div class="cont">
+              <h5>
+                <a href="/dark/project-details">
+                  Creative advertising in our life became a info noise
+                </a>
+              </h5>
+              <a
+                href="/dark/project-details"
+                class="d-flex align-items-center mt-30"
+              >
+                <span class="text mr-15">Read More</span>
+                <span class="ti-arrow-top-right"></span>
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </section>
 </template>
-
-<script setup>
-function handleMouseEnter(event) {
-  var tab_id = event.currentTarget.getAttribute('data-tab');
-
-  document.querySelectorAll('.links-text li').forEach((el) => {
-    el.classList.remove('current');
-    // el.classList.add('no-active');
-  });
-  document.querySelectorAll('.links-img .img').forEach((el) => {
-    el.classList.remove('current');
-    // el.classList.add('no-active');
-  });
-  event.currentTarget.classList.add('current');
-  // event.currentTarget.classList.remove('no-active');
-
-  document
-    .querySelectorAll('.links-img .img')
-    .forEach((el) => el.classList.remove('current'));
-  document.querySelector('#' + tab_id).classList.add('current');
-
-  if (event.currentTarget.classList.contains('current')) return false;
-}
-
-function handleMouseLeave() {
-  document
-    .querySelectorAll('.links-img .img')
-    .forEach((el) => el.classList.remove('current'));
-  document
-    .querySelectorAll('.links-text li')
-    .forEach((el) => el.classList.remove('current'));
-}
-</script>
+-->
