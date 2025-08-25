4d9e740c69ecfa3f5c764146b38664f0a11fed0b
Lautaro Maza <lautarodamian33@gmail.com>
2025-08-25 12:13:51 -0300

    soluciones

--- components/dark/About.vue
+++ components/dark/About.vue
@@ -1,71 +1,41 @@
 <template>
-  <section class="about">
-    <div class="container section-padding bord-thin-top">
-      <div class="row md-marg">
+  <div></div>
+</template>
+
+<script setup>
+// Componente comentado temporalmente
+</script>
+
+<!-- 
+<template>
+  <section class="about section-padding sub-bg">
+    <div class="container">
+      <div class="row">
         <div class="col-lg-6">
-          <div class="cont md-mb50">
-            <h6 class="sub-title main-color mb-15">Who We Are</h6>
-            <h3>
-              Our team consists ofnpractitioners who have years of experience in
-              launching & managing projects.
-            </h3>
-            <div class="d-flex align-items-end mt-100">
-              <div>
-                <a href="/dark/page-contact" class="d-flex align-items-end">
-                  <span class="sub-title nowrap">Contact Us</span>
-                  <span class="fz-70 line-height-45 ti-arrow-top-right"></span>
-                </a>
-              </div>
-              <div class="ml-80">
-                <p>
-                  With our low code solution you can model your process
-                  automation by yourself. This will increase your productivity
-                  in real time and bring all-around flexibility and
-                  best-in-class quality.
-                </p>
-              </div>
-            </div>
+          <div class="img">
+            <img src="/dark/assets/imgs/intro/2.jpg" alt="" />
           </div>
         </div>
         <div class="col-lg-6">
-          <div class="img-vid">
-            <img src="/dark/assets/imgs/intro/2.jpg" alt="" />
-            <div class="curv-butn main-bg">
-              <a href="https://youtu.be/AzwC6umvd1s" class="vid">
-                <div class="icon">
-                  <i class="fas fa-play"></i>
-                </div>
-              </a>
-              <div class="shap-left-top">
-                <svg
-                  viewBox="0 0 11 11"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="w-11 h-11"
-                >
-                  <path
-                    d="M11 1.54972e-06L0 0L2.38419e-07 11C1.65973e-07 4.92487 4.92487 1.62217e-06 11 1.54972e-06Z"
-                    fill="#1a1a1a"
-                  ></path>
-                </svg>
-              </div>
-              <div class="shap-right-bottom">
-                <svg
-                  viewBox="0 0 11 11"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="w-11 h-11"
-                >
-                  <path
-                    d="M11 1.54972e-06L0 0L2.38419e-07 11C1.65973e-07 4.92487 4.92487 1.62217e-06 11 1.54972e-06Z"
-                    fill="#1a1a1a"
-                  ></path>
-                </svg>
-              </div>
-            </div>
+          <div class="cont">
+            <h6 class="sub-title main-color mb-15">About Us</h6>
+            <h2 class="mb-30">
+              We are a creative studio that specializes in providing
+              high-quality design and branding solutions to businesses and
+              individuals.
+            </h2>
+            <p class="mb-30">
+              We provide digital experience services to startups and small
+              businesses. We help our clients succeed by creating brand
+              identities.
+            </p>
+            <a href="#0" class="btn-curve btn-lit mt-30">
+              <span>Get Started</span>
+            </a>
           </div>
         </div>
       </div>
     </div>
   </section>
 </template>
+-->
--- components/dark/about2/Services.vue
+++ components/dark/about2/Services.vue
@@ -1,156 +1,109 @@
 <template>
-  <section class="services-tab section-padding mb-80">
+  <div></div>
+</template>
+
+<script setup>
+// Componente comentado temporalmente
+</script>
+
+<!-- 
+<template>
+  <section class="services section-padding sub-bg">
     <div class="container">
-      <div class="row lg-marg" id="tabs">
-        <div class="col-lg-6 valign">
-          <div class="serv-tab-cont md-mb80">
-            <div class="tab-content current" id="tabs-1">
-              <div class="item">
-                <div class="img">
-                  <img src="/dark/assets/imgs/intro/03.jpg" alt="" />
-                </div>
-                <div class="cont sub-bg">
-                  <div class="icon-img-60 mb-40">
-                    <img src="/dark/assets/imgs/serv-icons/0.png" alt="" />
-                  </div>
-                  <div class="text">
-                    <p>
-                      We are a creative studio specializing in design,
-                      development and strategy many different skills and
-                      disciplines in the production of all web.
-                    </p>
-                  </div>
-                  <a href="/dark/page-services" class="mt-30">
-                    <span class="mr-15">Read More</span>
-                    <i class="fas fa-long-arrow-alt-right"></i>
-                  </a>
-                </div>
+      <div class="row">
+        <div class="col-lg-4">
+          <div class="item sub-bg mb-40">
+            <div class="img">
+              <img src="/dark/assets/imgs/intro/03.jpg" alt="" />
+              <div class="tag sub-bg">
+                <span>Envato</span>
               </div>
             </div>
-            <div class="tab-content" id="tabs-2">
-              <div class="item">
-                <div class="img">
-                  <img src="/dark/assets/imgs/intro/02.jpg" alt="" />
-                </div>
-                <div class="cont sub-bg">
-                  <div class="icon-img-60 mb-40">
-                    <img src="/dark/assets/imgs/serv-icons/1.png" alt="" />
-                  </div>
-                  <div class="text">
-                    <p>
-                      We are a creative studio specializing in design,
-                      development and strategy many different skills and
-                      disciplines in the production of all web.
-                    </p>
-                  </div>
-                  <a href="/dark/page-services" class="mt-30">
-                    <span class="mr-15">Read More</span>
-                    <i class="fas fa-long-arrow-alt-right"></i>
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div class="tab-content" id="tabs-3">
-              <div class="item">
-                <div class="img">
-                  <img src="/dark/assets/imgs/intro/03.jpg" alt="" />
-                </div>
-                <div class="cont sub-bg">
-                  <div class="icon-img-60 mb-40">
-                    <img src="/dark/assets/imgs/serv-icons/2.png" alt="" />
-                  </div>
-                  <div class="text">
-                    <p>
-                      We are a creative studio specializing in design,
-                      development and strategy many different skills and
-                      disciplines in the production of all web.
-                    </p>
-                  </div>
-                  <a href="/dark/page-services" class="mt-30">
-                    <span class="mr-15">Read More</span>
-                    <i class="fas fa-long-arrow-alt-right"></i>
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div class="tab-content" id="tabs-4">
-              <div class="item">
-                <div class="img">
-                  <img src="/dark/assets/imgs/intro/03.jpg" alt="" />
-                </div>
-                <div class="cont sub-bg">
-                  <div class="icon-img-60 mb-40">
-                    <img src="/dark/assets/imgs/serv-icons/0.png" alt="" />
-                  </div>
-                  <div class="text">
-                    <p>
-                      We are a creative studio specializing in design,
-                      development and strategy many different skills and
-                      disciplines in the production of all web.
-                    </p>
-                  </div>
-                  <a href="/dark/page-services" class="mt-30">
-                    <span class="mr-15">Read More</span>
-                    <i class="fas fa-long-arrow-alt-right"></i>
-                  </a>
-                </div>
-              </div>
+            <div class="cont">
+              <h5>
+                <a href="/dark/service-details">
+                  Creative advertising in our life became a info noise
+                </a>
+              </h5>
+              <a
+                href="/dark/service-details"
+                class="d-flex align-items-center mt-30"
+              >
+                <span class="text mr-15">Read More</span>
+                <span class="ti-arrow-top-right"></span>
+              </a>
             </div>
           </div>
         </div>
-        <div class="col-lg-6 valign">
-          <div class="serv-tab-link tab-links full-width pt-40">
-            <div class="sec-head mb-30">
-              <h6 class="sub-title mb-15 main-color">What we do?</h6>
-              <h2>The ultmiate guide to marketing success.</h2>
-            </div>
-            <div class="row justify-content-end">
-              <div class="col-lg-11">
-                <div class="text mb-50">
-                  <p>
-                    We shifted our talents to frontier science because we wanted
-                    our work to have tangible. we get front row seats to the
-                    future.
-                  </p>
-                </div>
-                <ul class="rest">
-                  <li
-                    @click="openTab"
-                    class="item-link current mb-15"
-                    data-tab="tabs-1"
-                  >
-                    <h4>
-                      <span class="fz-18 opacity-7 mr-15">01</span> UI-UX Design
-                    </h4>
-                  </li>
-                  <li
-                    @click="openTab"
-                    class="item-link mb-15"
-                    data-tab="tabs-2"
-                  >
-                    <h4>
-                      <span class="fz-18 opacity-7 mr-15">02</span> Web
-                      Development
-                    </h4>
-                  </li>
-                  <li
-                    @click="openTab"
-                    class="item-link mb-15"
-                    data-tab="tabs-3"
-                  >
-                    <h4>
-                      <span class="fz-18 opacity-7 mr-15">03</span>
-                      Digital Marketing
-                    </h4>
-                  </li>
-                  <li @click="openTab" class="item-link" data-tab="tabs-4">
-                    <h4>
-                      <span class="fz-18 opacity-7 mr-15">04</span>
-                      Product Design
-                    </h4>
-                  </li>
-                </ul>
+        <div class="col-lg-4">
+          <div class="item sub-bg mb-40">
+            <div class="img">
+              <img src="/dark/assets/imgs/intro/02.jpg" alt="" />
+              <div class="tag sub-bg">
+                <span>Envato</span>
               </div>
+            </div>
+            <div class="cont">
+              <h5>
+                <a href="/dark/service-details">
+                  Creative advertising in our life became a info noise
+                </a>
+              </h5>
+              <a
+                href="/dark/service-details"
+                class="d-flex align-items-center mt-30"
+              >
+                <span class="text mr-15">Read More</span>
+                <span class="ti-arrow-top-right"></span>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="item sub-bg mb-40">
+            <div class="img">
+              <img src="/dark/assets/imgs/intro/03.jpg" alt="" />
+              <div class="tag sub-bg">
+                <span>Envato</span>
+              </div>
+            </div>
+            <div class="cont">
+              <h5>
+                <a href="/dark/service-details">
+                  Creative advertising in our life became a info noise
+                </a>
+              </h5>
+              <a
+                href="/dark/service-details"
+                class="d-flex align-items-center mt-30"
+              >
+                <span class="text mr-15">Read More</span>
+                <span class="ti-arrow-top-right"></span>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="item sub-bg mb-40">
+            <div class="img">
+              <img src="/dark/assets/imgs/intro/03.jpg" alt="" />
+              <div class="tag sub-bg">
+                <span>Envato</span>
+              </div>
+            </div>
+            <div class="cont">
+              <h5>
+                <a href="/dark/service-details">
+                  Creative advertising in our life became a info noise
+                </a>
+              </h5>
+              <a
+                href="/dark/service-details"
+                class="d-flex align-items-center mt-30"
+              >
+                <span class="text mr-15">Read More</span>
+                <span class="ti-arrow-top-right"></span>
+              </a>
             </div>
           </div>
         </div>
@@ -158,12 +111,4 @@
     </div>
   </section>
 </template>
-<script setup>
-const openTab = (event) => {
-  document
-    .querySelectorAll('.tab-content')
-    .forEach((element) => (element.style.display = 'none'));
-  const tabid = event.currentTarget.getAttribute('data-tab');
-  document.querySelector(`.tab-content#${tabid}`).style.display = 'block';
-};
-</script>
+-->
